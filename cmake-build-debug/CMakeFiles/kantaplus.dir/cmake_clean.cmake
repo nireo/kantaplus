23944f81ec2c3bf644416c3307beb89c77daad50
nireo <eemil.lehtikangas1@gmail.com>
2021-04-20 21:52:45 +0300

    started the project

---
+++ cmake-build-debug/CMakeFiles/kantaplus.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kantaplus.dir/main.cpp.o"
+  "kantaplus"
+  "kantaplus.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kantaplus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
