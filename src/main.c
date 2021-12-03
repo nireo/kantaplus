89adf9215b1ee359b7fb6e7a9b19c7f0ac43faba
nireo <eemil.lehtikangas1@gmail.com>
2021-12-03 19:08:47 +0200

    added a makefile

--- src/main.c
+++ src/main.c
@@ -2,19 +2,19 @@
 #include <fcntl.h>
 
 int main(void) {
-    void *db;
+    kantadb_t db;
     if ((db = db_open("db_file", O_RDWR | O_CREAT | O_TRUNC, (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH))) == NULL) {
       error("error when db_open");
     }
 
     if (db_store(db, "test1", "data1", 1) != 0)
-      error_quit("db_store error when storing test1");
+      printf("error test 1");
 
     if (db_store(db, "test2", "data2", 2) != 0)
-      error_quit("db_store error when storing test2");
+      printf("error test 2");
 
     if (db_store(db, "test2", "data3", 3) != 0)
-      error_quit("db_store error when storing test3");
+      printf("error test 3");
 
     db_close(db);
     exit(0);
